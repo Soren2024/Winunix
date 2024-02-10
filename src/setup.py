aeadead2cdad7df85c58e7f79371c16cb0f16e62
Soren2024 <jankie2023@outlook.com>
2024-02-10 22:32:42 +0800

    Add tips message

--- src/setup.py
+++ src/setup.py
@@ -14,8 +14,9 @@ def accept(tips:str) -> bool:
 def setup() -> int:
   os.system('title Winunix Setup')
   print('{} WinUnix Setup {}'.format('*'*5,'*'*5))
-  
-
+  accept('This is an experimental project and the consequences are at your own risk.')
+  accept('The performance of this project varies from runtime to runtime, and I (Soren2024) will not be responsible if your computer is damaged.')
+  accept('I am capable of consent and agree to the above rules.')
 if 'win32' != sys.platform:
   print('You cannot run this script on non-Win32 platforms.')
   sys.exit(-1)
