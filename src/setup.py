894a37cc1edeb018c5e6ed3792cf674b8f55a83d
Soren2024 <jankie2023@outlook.com>
2024-02-10 22:27:48 +0800

    Switch to the setup function

--- src/setup.py
+++ src/setup.py
@@ -4,7 +4,10 @@ import os
 def setup() -> int:
   os.system('title Winunix Setup')
   print('{}WinUnix Setup'.format('*'*5,'*'*5))
+
 if 'win32' != sys.platform:
   print('You cannot run this script on non-Win32 platforms.')
   sys.exit(-1)
 
+if __name__ == '__main__':
+  setup()
