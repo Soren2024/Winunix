e7f1ab62e35b5801880b239dd54c3e352892189f
Soren2024 <jankie2023@outlook.com>
2024-02-10 22:30:31 +0800

    Add function for query

--- src/setup.py
+++ src/setup.py
@@ -1,9 +1,20 @@
 import sys
 import os
 
+def accept(tips:str) -> bool:
+  while True:
+    userInput = input(tips + '[Y/N]').lower()
+    if userInput != 'y' and userInput != 'n':
+      print('Invalid parameter, please reenter.')
+    elif userInput == 'y':
+      break
+    else:
+      sys.exit(0)
+
 def setup() -> int:
   os.system('title Winunix Setup')
   print('{} WinUnix Setup {}'.format('*'*5,'*'*5))
+  
 
 if 'win32' != sys.platform:
   print('You cannot run this script on non-Win32 platforms.')
